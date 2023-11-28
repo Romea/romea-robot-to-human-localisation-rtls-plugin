dc6a24e9270e3a5d35e49140391a19a9f3a3a467
Jean Laneurit <jean.laneurit@inrae.fr>
2023-11-28 14:43:10 +0100

    set diagnotic name equal to fully qualified  node name
    Signed-off-by: Jean Laneurit <jean.laneurit@inrae.fr>

--- src/robot_to_human_rtls_localisation_plugin.cpp
+++ src/robot_to_human_rtls_localisation_plugin.cpp
@@ -109,7 +109,8 @@ void R2HRTLSLocalisationPlugin::init_leader_position_publisher_()
 void R2HRTLSLocalisationPlugin::init_diagnostic_publisher_()
 {
   diagnostic_pub_ =
-    make_diagnostic_publisher<core::DiagnosticReport>(node_, node_->get_name(), 1.0);
+    make_diagnostic_publisher<core::DiagnosticReport>(
+    node_, node_->get_fully_qualified_name(), 1.0);
 }
 
 //-----------------------------------------------------------------------------
